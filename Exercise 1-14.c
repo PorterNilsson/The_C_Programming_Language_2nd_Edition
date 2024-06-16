6c564a68d5fad26c3bc342408f7fd4ae597ce7e5
PorterNilsson <92951219+PorterNilsson@users.noreply.github.com>
2024-06-16 16:08:15 -0600

    Solved Exercise 1-14

--- Exercise 1-14.c
+++ Exercise 1-14.c
@@ -8,45 +8,68 @@ in its input. */
 #define NEWLINE 2
 
 main() {
-    int i, c;
+    int i, j, c;
 
     /* We will have it track whitespace (spaces, tabs, and newlines), single digits, and letters (lowercase and capital). */
-    int nwhite[3], ndigit[10], nletter[52], nother;
+    int nwhite, ndigit[10], nlower[26], nupper[26], nother;
 
-    /* Initialize Arrays */
-    for (i = 0; i < 3; ++i) {
-        nwhite[i] = 0;
-    }
+    /* Initialize Values */
+    nwhite = nother = 0;
     for (i = 0; i < 10; ++i) {
         ndigit[i] = 0;
     }
-    for (i = 0; i < 52; ++i) {
-        nletter[i] = 0;
+    for (i = 0; i < 26; ++i) {
+        nlower[i] = 0;
+    }
+    for (i = 0; i < 26; ++i) {
+        nupper[i] = 0;
     }
 
     while ((c = getchar()) != EOF) {
         
-        if (c == ' ') {
-            ++nwhite[SPACE];
+        if (c == ' ' || c == '\t' || c == '\n') {
+            ++nwhite;
         }
-        if (c == '\t') {
-            ++nwhite[TAB];
+        else if (c >= '0' && c <= '9') {
+            ++ndigit[c - '0'];
         }
-        if (c == '\n') {
-            ++nwhite[NEWLINE];
+        else if (c >= 'a' && c <= 'z') {
+            ++nlower[c - 'a'];
         }
-        
-        for (i = 0; i < 10; ++i) {
-            if (c == i) {
-                ++ndigit[i];
-            }
+        else if (c >= 'A' && c <= 'Z') {
+            ++nupper[c - 'A'];
         }
-
-        for (i = 0; i < 52; ++i) {
-            if (c == ('a' + i)) {
-                
-            }
+        else {
+            ++nother;
         }
 
     }
+
+    printf("%3c|", 'w');
+    for (i = 0; i < nwhite; ++i) {
+        putchar('*');
+    }
+    putchar('\n');
+    for (i = 0; i < 10; ++i) {
+        printf("%3d|", i);
+        for (j = 0; j < ndigit[i]; ++j) {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    for (i = 0; i < 26; ++i) {
+        printf("%3c|", i + 'a');
+        for (j = 0; j < nlower[i]; ++j) {
+            putchar('*'); 
+        }
+        putchar('\n');
+    }
+    for (i = 0; i < 26; ++i) {
+        printf("%3c|", i + 'A');
+        for (j = 0; j < nupper[i]; ++j) {
+            putchar('*'); 
+        }
+        putchar('\n');
+    }
+
 }
