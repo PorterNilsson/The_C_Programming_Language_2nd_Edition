f8c519984b948f1a3420e1d4ba581ec4ebfa9311
PorterNilsson <92951219+PorterNilsson@users.noreply.github.com>
2024-11-20 11:27:29 -0700

    Solved 3-2

--- Chapter 3/Exercise 3-2/Exercise 3-2.c
+++ Chapter 3/Exercise 3-2/Exercise 3-2.c
@@ -2,43 +2,79 @@
 into visible escape sequences like \n and \t as it copies the string t to s. Use a switch. Write 
 a function for the other direction as well, converting escape sequences into the real characters. */
 
-/* I won't bother writing the function to convert back here because it's trivial, 
-and a conversion in either direction which conserves string size is impossible. It's not a great exercise. */
-
 #include <stdio.h>
 #include <string.h>
 
 void escape(char s[], char t[]);
+void re_escape(char s[], char t[]);
 
 int main(void) {
 
     char t[] = "Hello!\n\tHow are you?\n";
-    char s[strlen(t) + 1];
+    /* String s is contrived, but it needs to be large enough to 
+    account for 2 characters representing the escape sequences. */
+    char s[strlen(t) + 1 + 3];
 
     escape(s, t);
-
     printf("Original String: %s\n", t);
     printf("New String: %s\n", s);
+
+    printf("--------------------------------------\n");
+
+    re_escape(t, s);
+    printf("Original String: %s\n", s);
+    printf("New String: %s\n", t);
 
     return 0;
 }
 
 void escape(char s[], char t[]) {
-    unsigned int i = 0;
+    unsigned int i = 0, j = 0;
     while(t[i] != '\0') {
         switch(t[i]) {
             case '\n':
-                s[i] = '\\';
+                s[j] = '\\', s[j + 1] = 'n';
+                j++;
                 break;
             case '\t':
-                s[i] = '\\';
+                s[j] = '\\', s[j + 1] = 't';
+                j++;
                 break;
             default:
-                s[i] = t[i];
+                s[j] = t[i];
                 break;
         }
         i++;
+        j++;
     }
-    s[i] = '\0';
+    s[j] = '\0';
+    return;
+}
+
+void re_escape(char s[], char t[]) {
+    /* This is clearly not the best use case for a switch statement as 
+    identifying an escape character requires two indicies */
+    unsigned int i = 0, j = 0;
+    while (t[i] != '\0') {
+        switch(t[i]) {
+            case '\\':
+                if (t[i + 1] == 'n') {
+                    s[j] = '\n';
+                    i++;
+                }
+                else if (t[i + 1] == 't') {
+                    s[j] = '\t';
+                    i++;
+                }
+                else {
+                    s[j] = t[i];
+                }
+                break;
+            default:
+                s[j] = t[i];
+        }           
+        ++i, ++j;
+    }
+    s[j] = '\0';
     return;
 }
